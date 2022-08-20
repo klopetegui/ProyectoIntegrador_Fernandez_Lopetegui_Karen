98df15e53367832c3d8f88cc9c31cacb2dc74c70
klopetegui <karenfernandezlopetegui@gmail.com>
2022-08-20 17:19:37 -0300

    Creacion de BackEnd

---
+++ ProyectoIntegrador_Fernandez_Lopetegui_Karen/BackEnd/src/test/java/com/portfolio/KFL/KflApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.KFL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KflApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
